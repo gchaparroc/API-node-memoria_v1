437ed6929930432827b55094d01405fb2ba38c89
gchaparroc <gchaparroc.certificaciones@gmail.com>
2022-07-01 16:06:18 -0400

    moviendo cors

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const cors = require('cors');
 const routerApi = require('./routes');
 
 const app = express();
+app.use(cors());              //usamos libreria que descargamos de CORS (middleware)
+
 const port = 3000;         //este es el puesto 3000 donde corre mi app
 
 app.use(express.json());      //implementacion middleware
-app.use(cors());              //usamos libreria que descargamos de CORS (middleware)
 
 app.get('/', (req, res) => {
   res.send("Hola mi server en Express");
